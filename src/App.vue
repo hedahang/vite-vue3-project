4659bab032e7331cd472247b6e4cd4ae3a2aeb72
munachar <hdh_angular@163.com>
2026-01-05 14:06:23 +0800

    feat: 添加 rollup-plugin-visualizer 以生成打包报告，并更新相关依赖项

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import { ref } from "vue";
+// function fib(n: number): number {
+//   if (n <= 1) return 1;
+//   return fib(n - 1) + fib(n - 2);
+// }
+// function fib(n: number): number {
+//   let arr = [1, 1];
+//   let i = 2;
+//   while (i <= n) {
+//     arr[i] = (arr[i - 1] || 0) + (arr[i - 2] || 0);
+//     i++;
+//   }
+//   return arr[n] || 0;
+// }
+// let count = ref(fib(38));
+// console.log(count.value);
+</script>
 
 <template>
   <div id="app">
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import { viteMockServe } from "vite-plugin-mock";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -14,6 +15,12 @@ export default defineConfig(({ mode }) => {
         mockPath: "src/mock", // mock 文件存放目录
         enable: env.VITE_USE_MOCK === "true", // 根据环境变量控制
         watchFiles: true, // 监听文件变化
+      }),
+      visualizer({
+        filename: "dist/stats.html", // 输出报告
+        open: true, // 打包后自动打开
+        gzipSize: true, // 显示 gzip 体积
+        brotliSize: true, // 显示 brotli 体积
       }),
     ],
   };
